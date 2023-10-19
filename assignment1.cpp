1e9000065bea6de52e40a41ea5da82f0a1dec0dc
Sabeen Ahmad <sabeen231@gmail.com>
2023-10-19 15:40:25 -0500

    created function for creating Matrix

--- assignment1.cpp
+++ assignment1.cpp
@@ -3,6 +3,34 @@
 #include <string>
 using namespace std;
 
+int creatingMatrix(float matrixA[100][100], float matrixB[100][100]){
+    ifstream file("text.txt");
+    if (!file.is_open()){
+        cout<<"No file found"<< endl;
+        return 1;
+    }
+    int size;
+    file>>size;
+    cout<<"\nMatrix A"<<endl;
+    for (int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+           file >> matrixA[i][j];
+           cout << matrixA[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout<<"\nMatrix B"<<endl;
+     for (int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+           file >> matrixB[i][j];
+           cout << matrixB[i][j] << " ";
+        }
+        cout << endl;
+    }
+    file.close();
+    return size;
+}
+
 void addition(float matrixA[100][100], float matrixB[100][100], int matrixSize){
     cout<< "\nMatrix Addition between Matrix A and B"<<endl;
     float newMatrix[100][100];
@@ -48,30 +76,7 @@ int main() {
 
     float matrixA [100][100];
     float matrixB[100][100];
-    ifstream file("text.txt");
-    if (!file.is_open()){
-        cout<<"No file found"<< endl;
-        return 1;
-    }
-    int size;
-    file>>size;
-    cout<<"\nMatrix A"<<endl;
-    for (int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++){
-           file >> matrixA[i][j];
-           cout << matrixA[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout<<"\nMatrix B"<<endl;
-     for (int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++){
-           file >> matrixB[i][j];
-           cout << matrixB[i][j] << " ";
-        }
-        cout << endl;
-    }
-    file.close();
+    int size = creatingMatrix(matrixA,matrixB);
     addition(matrixA,matrixB,size);
     multiplication(matrixA,matrixB,size);
     difference(matrixA,matrixB,size);
